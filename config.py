a168580eed4bf51e55b02a7dc5de1aaff1110b78
chance <cchen@indocresearch.org>
2021-07-26 21:49:54 -0400

    VRE deployment

--- config.py
+++ config.py
@@ -1,3 +1,20 @@
 
+import os
+
 class ConfigClass:
     api_modules = ["api"]
+    env = os.environ.get('env', 'test')
+    if env == "staging":
+        # staging config
+        VAULT_SERVICE = "https://vault.vault:8200/v1/vre/app/config"
+        VAULT_CRT="/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+        VAULT_TOKEN="s.RISGjSyH1pylevR2Mq2gL2BC"
+    elif env == "charite":
+        # charite config
+        VAULT_SERVICE = "https://vault.vault:8200/v1/vre/app/config"
+        VAULT_CRT="/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+        VAULT_TOKEN="s.Juas29svaEwuOwThii3h5t5F"
+    else:
+        VAULT_SERVICE = "https://vault.vault:8200/v1/vre/app/config"
+        VAULT_CRT="/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+        VAULT_TOKEN="s.VCX1vOVI94q1YKoYi4OZXpxz"
---
+++ models/config_center_policy.py
@@ -0,0 +1,24 @@
+class ConfigCenterPolicy:
+
+    @staticmethod
+    def get_granted(srv_namespace):
+        return {
+            'service_download': ConfigCenterPolicy.service_download()
+        }.get(
+            srv_namespace,
+            []
+        )
+
+    @staticmethod
+    def service_download():
+        return [
+            "DOWNLOAD",
+            "KEYCLOAK",
+            "MINIO",
+            "REDIS",
+            "NEO4J_SERVICE",
+            "PROVENANCE_SERVICE",
+            "QUEUE_SERVICE",
+            "UTILITY_SERVICE",
+            "DATA_OPS_GR",
+        ]
