170f349fdc76cbbb014d9cf5b1aea8d628f2935a
Mohammad <mnaghavi@indocresearch.org>
2021-11-04 16:34:40 -0400

    VRE deployment

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ class ConfigClass:
         # staging config
         VAULT_SERVICE = "https://vault.vault:8200/v1/vre/app/config"
         VAULT_CRT = "/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-        VAULT_TOKEN = "s.zHgLC3aJKHLcFA12PG96hQ8x"
+        VAULT_TOKEN = "s.L35l8Ip6lgLikVJK4CaktfrE"
     elif env == "charite":
         # charite config
         VAULT_SERVICE = "https://vault.vault:8200/v1/vre/app/config"
         VAULT_CRT = "/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-        VAULT_TOKEN = "s.k58KFHp09AUnSSUlaNZI76IL"
+        VAULT_TOKEN = "s.NPjdx6Rb2UfptYOjVQ7LPWIL"
     else:
         VAULT_SERVICE = "https://vault.vault:8200/v1/vre/app/config"
         VAULT_CRT = "/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-        VAULT_TOKEN = "s.uxoIjrydEZsgTF0zBWdsiVJ9"
+        VAULT_TOKEN = "s.p97lqlxBZ61C6sx5aHAFB4Ya"
