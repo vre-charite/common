f6653af8c4031ef9dfed5a6d09866dda05b7340b
Mohammad <mnaghavi@indocresearch.org>
2021-12-07 16:44:22 -0500

    VRE deployment

--- config.py
+++ config.py
@@ -1,20 +1,48 @@
-import os
+from pydantic import BaseSettings, Extra
+from typing import Dict, Set, List, Any
+from functools import lru_cache
 
 
-class ConfigClass:
-    api_modules = ["api"]
-    env = os.environ.get('env', 'test')
-    if env == "staging":
-        # staging config
-        VAULT_SERVICE = "https://vault.vault:8200/v1/vre/app/config"
-        VAULT_CRT = "/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-        VAULT_TOKEN = "s.L35l8Ip6lgLikVJK4CaktfrE"
-    elif env == "charite":
-        # charite config
-        VAULT_SERVICE = "https://vault.vault:8200/v1/vre/app/config"
-        VAULT_CRT = "/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-        VAULT_TOKEN = "s.NPjdx6Rb2UfptYOjVQ7LPWIL"
-    else:
-        VAULT_SERVICE = "https://vault.vault:8200/v1/vre/app/config"
-        VAULT_CRT = "/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-        VAULT_TOKEN = "s.p97lqlxBZ61C6sx5aHAFB4Ya"
+class Settings(BaseSettings):
+    port: int = 5001
+    host: str = "127.0.0.1"
+    env: str = "test"
+    namespace: str = ""
+    
+    api_modules: List[str] = ["api"]
+    VAULT_SERVICE: str = "https://vault.vault:8200/v1/vre/app/config"
+    VAULT_CRT: str = "/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+    VAULT_TOKEN: str
+    
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+        extra = Extra.allow
+
+        @classmethod
+        def customise_sources(
+            cls,
+            init_settings,
+            env_settings,
+            file_secret_settings,
+        ):
+            return (
+                env_settings,
+                init_settings,
+                file_secret_settings,
+            )
+    
+
+@lru_cache(1)
+def get_settings():
+    settings =  Settings()
+    return settings
+
+class ConfigClass(object):
+    settings = get_settings()
+    env = settings.env
+    api_modules = settings.api_modules
+
+    VAULT_SERVICE = settings.VAULT_SERVICE
+    VAULT_CRT = settings.VAULT_CRT
+    VAULT_TOKEN = settings.VAULT_TOKEN
--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from app import create_app
+from config import ConfigClass
 
 flaskapp = create_app()
 
 # add to https
 if __name__ == '__main__':
-    flaskapp.run(debug=True, port=5001)
+    flaskapp.run(debug=True, port=ConfigClass.settings.port)
